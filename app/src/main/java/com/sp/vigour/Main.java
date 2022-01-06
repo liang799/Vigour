274d83759207e170007069ecf8df9982e2d61806
liang799 <neohtp@tutanota.com>
2022-01-06 12:56:01 +0800

    Added fragments and activities

---
+++ app/src/main/java/com/sp/vigour/Main.java
@@ -0,0 +1,23 @@
+package com.sp.vigour;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment_container);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
