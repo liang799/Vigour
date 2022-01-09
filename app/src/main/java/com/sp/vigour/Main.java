a2b9e97b194db5499d77b663f657362e0e782524
liang799 <neohtp@tutanota.com>
2022-01-09 19:14:44 +0800

    Changed logo of our app and added toolbar

--- app/src/main/java/com/sp/vigour/Main.java
+++ app/src/main/java/com/sp/vigour/Main.java
@@ -1,6 +1,7 @@
 package com.sp.vigour;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -15,6 +16,8 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,  R.id.fragment_container);
