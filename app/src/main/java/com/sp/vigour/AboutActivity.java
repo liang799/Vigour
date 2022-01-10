81273b0a9fc05ce8ee286d380a9e3365108879ca
liang799 <neohtp@tutanota.com>
2022-01-10 12:22:58 +0800

    Added raja's stuff

---
+++ app/src/main/java/com/sp/vigour/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.sp.vigour;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AboutActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_about,container,false);
+        return v;
+    }
+}
