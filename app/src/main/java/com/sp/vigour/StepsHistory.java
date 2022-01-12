20b89a990ecaa966deb8e9448ea82072d52defbb
liang799 <neohtp@tutanota.com>
2022-01-12 23:01:13 +0800

    steps_row.xml complete

--- app/src/main/java/com/sp/vigour/StepsHistory.java
+++ app/src/main/java/com/sp/vigour/StepsHistory.java
@@ -3,62 +3,35 @@ package com.sp.vigour;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StepsHistory#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StepsHistory extends Fragment {
+    public StepsHistory() {}
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerView;
+    private TextView steps_text;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StepsHistory() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StepsHistory.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StepsHistory newInstance(String param1, String param2) {
-        StepsHistory fragment = new StepsHistory();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    // Testing data
+    int steps[] = {9000, 8000, 1000, 200, 500, 0};
+    String time[] = {"8:35pm", "Yesterday", "January, 10", "January, 09", "January, 08", "January, 07"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_steps_history, container, false);
+        View v = inflater.inflate(R.layout.fragment_steps_history, container, false);
+        recyclerView = (RecyclerView) v.findViewById(R.id.steps_history);
+        steps_text = (TextView) v.findViewById(R.id.steps_hist_main);
+
+        return v;
     }
 }
