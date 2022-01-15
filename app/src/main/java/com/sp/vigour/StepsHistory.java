dea8679c915da81fd7654ce9ceb513f7ee7950a1
liang799 <neohtp@tutanota.com>
2022-01-16 01:39:39 +0800

    User is now able to close StepsHistory

--- app/src/main/java/com/sp/vigour/StepsHistory.java
+++ app/src/main/java/com/sp/vigour/StepsHistory.java
@@ -4,24 +4,29 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
-import java.util.function.ToLongBiFunction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class StepsHistory extends Fragment {
+import java.util.logging.Logger;
+
+public class StepsHistory extends Fragment implements View.OnClickListener {
     public StepsHistory() {}
 
+    private BottomNavigationView navBar;
     private RecyclerView recyclerView;
     private TextView steps_text;
+    private ImageButton close;
 
     // Testing data
     int steps[] = {9000, 8000, 1000, 200, 500, 0};
@@ -36,9 +41,11 @@ public class StepsHistory extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_steps_history, container, false);
-        setHasOptionsMenu(true);
+        navBar = getActivity().findViewById(R.id.bottomNavigationView);
         recyclerView = (RecyclerView) v.findViewById(R.id.steps_history);
         steps_text = (TextView) v.findViewById(R.id.steps_hist_main);
+        close = (ImageButton) v.findViewById(R.id.close_sHist);
+        close.setOnClickListener(this);
 
         return v;
     }
@@ -47,11 +54,24 @@ public class StepsHistory extends Fragment {
     public void onResume() {
         super.onResume();
         ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        navBar.setVisibility(View.GONE);
     }
 
     @Override
     public void onStop() {
         super.onStop();
         ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+        navBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.close_sHist:
+                Navigation.findNavController(v).navigate(R.id.action_stepsHistory_to_steps);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
     }
 }
