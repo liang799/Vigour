63cfeb795eafdf1014ef57d25d4af1384fb74c8f
liang799 <neohtp@tutanota.com>
2022-01-16 00:26:03 +0800

    goto_step_hist button working

--- app/src/main/java/com/sp/vigour/Steps.java
+++ app/src/main/java/com/sp/vigour/Steps.java
@@ -1,18 +1,26 @@
 package com.sp.vigour;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class Steps extends Fragment {
+import java.io.ByteArrayOutputStream;
+
+public class Steps extends Fragment implements View.OnClickListener {
+    private Button stepsButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,19 @@ public class Steps extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_steps, container, false);
+        stepsButton = (Button) v.findViewById(R.id.goto_steps_hist);
+        stepsButton.setOnClickListener(this);
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.goto_steps_hist:
+                Navigation.findNavController(v).navigate(R.id.action_steps_to_stepsHistory);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
     }
 }
