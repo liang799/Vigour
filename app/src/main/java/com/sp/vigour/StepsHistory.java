d2fa3f9dc360a4b31d12eb36ea38e58e77f51612
liang799 <neohtp@tutanota.com>
2022-01-16 01:18:42 +0800

    Managed to hide toolbar in fragment_steps_history.xml

--- app/src/main/java/com/sp/vigour/StepsHistory.java
+++ app/src/main/java/com/sp/vigour/StepsHistory.java
@@ -2,13 +2,20 @@ package com.sp.vigour;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.util.function.ToLongBiFunction;
 
 public class StepsHistory extends Fragment {
     public StepsHistory() {}
@@ -29,9 +36,22 @@ public class StepsHistory extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_steps_history, container, false);
+        setHasOptionsMenu(true);
         recyclerView = (RecyclerView) v.findViewById(R.id.steps_history);
         steps_text = (TextView) v.findViewById(R.id.steps_hist_main);
 
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
     }
 }
