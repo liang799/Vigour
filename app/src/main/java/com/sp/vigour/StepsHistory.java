96df214fdf1efc3837ec0fea2e3ee3d21253b625
liang799 <neohtp@tutanota.com>
2022-01-16 02:23:09 +0800

    Added RecycerView adapter for testing purposes

--- app/src/main/java/com/sp/vigour/StepsHistory.java
+++ app/src/main/java/com/sp/vigour/StepsHistory.java
@@ -1,24 +1,19 @@
 package com.sp.vigour;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.logging.Logger;
 
 public class StepsHistory extends Fragment implements View.OnClickListener {
     public StepsHistory() {}
@@ -42,10 +37,13 @@ public class StepsHistory extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_steps_history, container, false);
         navBar = getActivity().findViewById(R.id.bottomNavigationView);
-        recyclerView = (RecyclerView) v.findViewById(R.id.steps_history);
-        steps_text = (TextView) v.findViewById(R.id.steps_hist_main);
         close = (ImageButton) v.findViewById(R.id.close_sHist);
         close.setOnClickListener(this);
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.steps_history);
+        StepsAdapter adapter = new StepsAdapter(getActivity(), steps, time);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return v;
     }
