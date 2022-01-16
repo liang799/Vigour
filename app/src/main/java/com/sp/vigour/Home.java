4efbdf5dc15c00c1661bb3802ff5792352a97f52
liang799 <neohtp@tutanota.com>
2022-01-16 16:26:07 +0800

    User can now hide amount in Home Frag

--- app/src/main/java/com/sp/vigour/Home.java
+++ app/src/main/java/com/sp/vigour/Home.java
@@ -1,64 +1,61 @@
 package com.sp.vigour;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Home#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Home extends Fragment {
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class Home extends Fragment implements View.OnClickListener {
+    private Button detailsButt;
+    private TextView tips;
+    private TextView Vgr_Amount;
+    private TextView hide_indicator;
+    private TextView toggle_hide;
+    private boolean hidden = false;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Home() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Home newInstance(String param1, String param2) {
-        Home fragment = new Home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        tips = view.findViewById(R.id.tips);
+        detailsButt = view.findViewById(R.id.details_button);
+        detailsButt.setOnClickListener(this);
+        Vgr_Amount = view.findViewById(R.id.balance_home);
+        hide_indicator = view.findViewById(R.id.hideEye);
+        toggle_hide = view.findViewById(R.id.visibilitySwitch);
+        toggle_hide.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.visibilitySwitch:
+                if (hidden == false) {
+                    hide_indicator.setVisibility(View.VISIBLE);
+                    Vgr_Amount.setText("***");
+                    hidden = true;
+                } else {
+                    hide_indicator.setVisibility(View.GONE);
+                    Vgr_Amount.setText("100");
+                    hidden = false;
+                }
+                break;
+            case R.id.details_button:
+                Navigation.findNavController(v).navigate(R.id.action_home2_to_transactions);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
     }
 }
