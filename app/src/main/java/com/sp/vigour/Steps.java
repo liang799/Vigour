17f542373a6396615bb2faa8ef9320201f8ef663
raja <rajaselvam2003@gmail.com>
2022-01-17 16:55:31 +0800

    madehistory button go to steps history

--- app/src/main/java/com/sp/vigour/Steps.java
+++ app/src/main/java/com/sp/vigour/Steps.java
@@ -1,14 +1,19 @@
 package com.sp.vigour;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.common.data.DataBufferUtils;
 
 import org.w3c.dom.Text;
 
@@ -23,6 +28,17 @@ public class Steps extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_steps, container, false);
+
+        Button historyview = v.findViewById(R.id.button3);
+
+        historyview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container,new StepsHistory() );
+                fr.commit();
+            }
+        });
 
 
         return v;
