2dc5174e770563c037c5499d8a59957b65e6c10e
liang799 <neohtp@tutanota.com>
2022-01-20 22:42:45 +0800

    Renamed DebuggingCounter to Debugging

---
+++ app/src/main/java/com/sp/vigour/Debugging.java
@@ -0,0 +1,25 @@
+package com.sp.vigour;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Debugging extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_debugging_counter, container, false);
+
+        return v;
+    }
+}
