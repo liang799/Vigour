1758adbcf633d272c33c078750ac38f3107fd4a4
liang799 <neohtp@tutanota.com>
2022-01-28 20:38:40 +0800

    Who deleted Transactions.java?

---
+++ app/src/main/java/com/sp/vigour/Transactions.java
@@ -0,0 +1,79 @@
+package com.sp.vigour;
+
+import android.database.Cursor;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class Transactions extends Fragment {
+
+    ArrayList<Vigouritem> vigouritemArrayList;
+    ArrayList<String> historyID;
+    Addhelper db;
+
+    CustomadapterSteps customadapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_transactions, container, false);
+
+        RecyclerView historyview = v.findViewById(R.id.historyview);
+
+        db = new Addhelper(getContext());
+
+        historyID = new ArrayList<>();
+        vigouritemArrayList = new ArrayList<>();
+
+        storeDatainArray();
+
+
+        customadapter = new CustomadapterSteps(getContext(),historyID, vigouritemArrayList);
+        historyview.setAdapter(customadapter);
+        historyview.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        return v;
+    }
+
+    private void storeDatainArray() {
+
+        db.close();
+        Cursor cursor = db.getdata();
+
+        historyID.clear();
+
+
+        vigouritemArrayList.clear();
+
+        if(cursor.getCount() == 0){
+            Toast.makeText(getContext(), "No data", Toast.LENGTH_SHORT).show();
+        }else {
+            while (cursor.moveToNext()){
+
+                historyID.add(cursor.getString(0));
+
+                String usersteps = (cursor.getString(1));
+                String userdate = (cursor.getString(2));
+                String usertime = (cursor.getString(3));
+                String usercrypto = (cursor.getString(4));
+
+
+                vigouritemArrayList.add(new Vigouritem( usersteps, userdate, usertime, usercrypto));
+
+            }
+            db.close();
+        }
+    }
+}
