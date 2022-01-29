525f123e3232bd1db5ab12d7d34b230f225375b3
liang799 <neohtp@tutanota.com>
2022-01-29 13:38:20 +0800

    WTF Fazith. Why u delete Map?

---
+++ app/src/main/java/com/sp/vigour/Map.java
@@ -0,0 +1,24 @@
+package com.sp.vigour;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Map extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        return v;
+    }
+}
