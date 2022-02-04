f14b43223853d6e011f3a4e59d04e2212e57c7e7
liang799 <neohtp@tutanota.com>
2022-02-04 22:57:59 +0800

    Optimized imports for LoginActivity

--- app/src/main/java/com/sp/vigour/activities/LoginActivity.java
+++ app/src/main/java/com/sp/vigour/activities/LoginActivity.java
@@ -5,26 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.shobhitpuri.custombuttons.GoogleSignInButton;
 import com.sp.vigour.R;
 
 import org.web3j.crypto.Credentials;
@@ -79,9 +63,8 @@ public class LoginActivity extends AppCompatActivity {
             else
                 Log.d("Wallet", "Directory exists");
             Walletname = WalletUtils.generateLightNewWalletFile(password, file);
-            Toast.makeText(getApplicationContext(), "Wallet generated wallet name is" + Walletname, Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Wallet generated wallet name is " + Walletname, Toast.LENGTH_SHORT).show();
             credentials = WalletUtils.loadCredentials(password, file + "/" + Walletname);
-            Log.d("Wallet","Wallet generated wallet name is" + Walletname );
         } catch(Exception e) {
             e.printStackTrace();
             Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
