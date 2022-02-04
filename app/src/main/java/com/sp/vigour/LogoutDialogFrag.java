f9ee95e370a8f288f836131dd8b8730f14c03927
liang799 <neohtp@tutanota.com>
2022-02-04 22:32:14 +0800

    Removed google signout

--- app/src/main/java/com/sp/vigour/LogoutDialogFrag.java
+++ app/src/main/java/com/sp/vigour/LogoutDialogFrag.java
@@ -6,14 +6,11 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.navigation.Navigation;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogoutDialogFrag extends DialogFragment {
@@ -32,19 +29,8 @@ public class LogoutDialogFrag extends DialogFragment {
         builder.setMessage("Do you wish to logout of Vigour?")
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()){
-                                    firebaseAuth.signOut();
-
-                                    Toast.makeText(getActivity().getApplicationContext()
-                                            ,"Logout Successful", Toast.LENGTH_SHORT).show();
-
-                                    Navigation.findNavController(getActivity(), R.id.fragment_container).navigate(R.id.action_logout_to_loginActivity);
-                                }
-                            }
-                        });
+                        Toast.makeText(getActivity().getApplicationContext(),"Logout Successful", Toast.LENGTH_SHORT).show();
+                        Navigation.findNavController(getActivity(), R.id.fragment_container).navigate(R.id.action_logout_to_loginActivity);
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
